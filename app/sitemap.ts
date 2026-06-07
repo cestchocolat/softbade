c7fddabd74fb2670f5add5bf4b87dc9d60e392e6
Kamonchanok Saekong <tia@TiaTia.local>
2026-06-07 16:26:31 +0700

    Expand sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,5 +14,30 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://softbade.com/categories",
       lastModified: new Date(),
     },
+
+    {
+      url: "https://softbade.com/categories/ai-automation",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softbade.com/categories/crm-sales",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softbade.com/categories/design-creative",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softbade.com/categories/finance-tools",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softbade.com/categories/marketing-seo",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softbade.com/categories/productivity",
+      lastModified: new Date(),
+    },
   ];
 }
